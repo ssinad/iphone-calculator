af0f350fb3594182f92f601fc46a12ea4a038213
ssd352 <27953063+ssd352@users.noreply.github.com>
2016-01-13 08:16:02 +0330

    UI almost complete.

--- iPhone Calculator/ViewController.m
+++ iPhone Calculator/ViewController.m
@@ -9,13 +9,27 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+-(void)setResultLabelText:(NSString*) number;
 @end
 
 @implementation ViewController
-
+- (IBAction)clearResultLabel:(UIButton *)sender {
+    self.resultLabel.text = @"0";
+    [self.clearButton setTitle:@"AC" forState:UIControlStateNormal];
+}
+-(void)setResultLabelText:(NSString *)number{
+    if ([self.resultLabel.text isEqualToString:@"0"]){
+        self.resultLabel.text = number;
+    }
+    else{
+        self.resultLabel.text = [self.resultLabel.text stringByAppendingString:number];
+    }
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    NSExpression * expression = [NSExpression expressionWithFormat:@"2+2="];
+//    NSLog(@"%@", expression);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +37,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//- (IBAction)NumberButtonDidTouch:(id)sender {
+//    if (! [sender isKindOfClass:[UIButton class]])
+//        return;
+//    [self setResultLabelText:((UIButton *) sender).titleLabel.text];
+//}
+- (IBAction)numberButtonDidTouch:(UIButton *)sender {
+    [self setResultLabelText:sender.titleLabel.text];
+    if (![sender.titleLabel.text isEqualToString:@"0"])
+        [self.clearButton setTitle:@"C" forState:UIControlStateNormal];
+    
+}
+
+- (IBAction)pointButtonDidTouch:(UIButton *)sender {
+    if (! [self.resultLabel.text containsString:@"."])
+        self.resultLabel.text = [self.resultLabel.text stringByAppendingString:@"."];
+}
+
 
 @end
